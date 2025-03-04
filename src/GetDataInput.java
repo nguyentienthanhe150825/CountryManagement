ee65e72789b961dae37bfd2f1855d1ea0d89ad5f
nguyentienthanhe150825 <nguyenthanh2542001@gmail.com>
2025-03-04 13:20:45 +0700

    Display Menu

---
+++ src/GetDataInput.java
@@ -0,0 +1,7 @@
+public class GetDataInput {
+
+    public static int inputOption(String message, String error, int min, int max) {
+        return 5;
+    }
+    
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,53 @@
 
 public class Main {
     public static void main(String[] args) {
-        
+        ManageEastAsiaCountries manageEastAsiaCountries = new ManageEastAsiaCountries();
+
+        while (true) {
+            // Display menu
+            Main.displayMenu();
+
+            // Enter option
+            int option = GetDataInput.inputOption("Enter your option: ", "Please input number!", 1, 5);
+
+            // Select option
+            switch (option) {
+                // Add country
+                case 1:
+                
+                    break;
+
+                // Display list country
+                case 2:
+
+                    break;
+
+                // Search country by name
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                // Exit
+                case 5:
+                    System.exit(0);
+                    break;
+
+            }
+        }
+    }
+
+    private static void displayMenu() {
+        System.out.println("                               MENU\n"
+                + "==========================================================================\n"
+                + "1. Input the information of 5 countries in East Asia\n"
+                + "2. Display the information of country you've just input\n"
+                + "3. Search the information of country by user-entered name\n"
+                + "4. Display the information of countries sorted name in ascending order  \n"
+                + "5. Exit \n"
+                + "==========================================================================");
     }
 }
