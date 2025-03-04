8bed5cfbdfe2f6a4d1f435b5b131b11124ffab91
nguyentienthanhe150825 <nguyenthanh2542001@gmail.com>
2025-03-04 13:06:43 +0700

    Init project

---
+++ src/EastAsiaCountries.java
@@ -0,0 +1,25 @@
+
+public class EastAsiaCountries extends Country {
+    private String countryTerrain;
+
+    public EastAsiaCountries(String countryCode, String countryName, float totalArea, String countryTerrain) {
+        super(countryCode, countryName, totalArea);
+        this.countryTerrain = countryTerrain;
+    }
+
+    public String getCountryTerrain() {
+        countryTerrain = countryTerrain.substring(0, 1).toUpperCase() + countryTerrain.substring(1).toLowerCase();
+        return countryTerrain;
+    }
+
+    public void setCountryTerrain(String countryTerrain) {
+        this.countryTerrain = countryTerrain;
+    }
+
+    @Override
+    public void display() {
+        System.out.format("%-20s%-20s%-20s%-20s\n", this.getCountryCode(), this.getCountryName(), this.getTotalArea(),
+                this.getCountryTerrain());
+    }
+
+}
